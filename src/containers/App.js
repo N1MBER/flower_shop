ffcaa57004e72b1533be4740a8d9a7efcf425c2d
Maxim <socialfox33@gmail.com>
2020-07-26 20:01:57 +0300

    Finished version of main page on desktop

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../App.css';
+import '../styles/App.css';
 import {Header} from "./Header";
 import {NavBar} from "../components/NavBar";
 import {
--- src/containers/MainPage.js
+++ src/containers/MainPage.js
@@ -1,10 +1,40 @@
 import React from "react";
 
+let orders =[
+    ["ЗАКАЖИТЕ", "ЦВЕТЫ", "Купите любимым"],
+    ["ЗАКАЖИТЕ", "НАБОР", "Сделать подарок"],
+    ["ПОСМОТРИТЕ", "ГАЛЕРЕЯ", "Просмотреть портфолио"]
+];
+let links = ["/products/flowers", "/products/set", "https://github.com/N1MBER"];
+
 export class MainPage extends React.Component{
     render() {
         return(
-            <div>
-                main
+            <div id={"main_content"}>
+                <div>
+                    {orders.map(function (item) {
+                        return (
+                            <div className={"order_element"}>
+                                <p>{item[0]}</p>
+                                <h2>{item[1]}</h2>
+                                :::::::::::::::::::::
+                                <button>
+                                    <p>{item[2]}</p>
+                                    <img width={30} alt={""} src={"https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/Circle_arrow_right_font_awesome.svg/768px-Circle_arrow_right_font_awesome.svg.png"}/>
+                                </button>
+                            </div>
+                        )
+                    })}
+                </div>
+                <div className={"main_photo"}>
+                    <div>
+                        <h1>Order <span>Flowers</span></h1>
+                        <hr/>
+                        <p>100% Гарантия</p>
+                        <hr/>
+                        <button>Заказать сейчас ></button>
+                    </div>
+                </div>
             </div>
         )
     }
